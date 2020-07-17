9afd5a7371b28da71aacd518d0a54ffa095af342
Joseph Odhiambo <josephkdo@gmail.com>
2020-07-17 18:21:39 +0300

    day function calculation

--- js/myScript.js
+++ js/myScript.js
@@ -34,3 +34,17 @@ function validate() {
   
 }
 
+
+//calculation for the day value
+function calculateDayValue(){
+    year = document.getElementById("year").value;
+    CC = parseInt(year.substring(0,2));
+    YY = parseInt(year.substring(2,4));
+    MM = parseInt(document.getElementById("month").value);
+    DD = parseInt(document.getElementById("date").value);
+    d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+    console.log(d);
+    return (Math.floor(d));
+  }
+  
+  
