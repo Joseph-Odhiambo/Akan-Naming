f16a6d1fc829810ce8fa41cf52174977cf626367
Joseph Odhiambo <josephkdo@gmail.com>
2020-07-17 18:49:42 +0300

    Reset button addition

--- js/myScript.js
+++ js/myScript.js
@@ -3,6 +3,7 @@ var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var output = document.getElementById("results");
 
 // input vadidation
 function validate() {
@@ -62,47 +63,47 @@ function calculateDayValue(){
     switch(gender){
       case "male":
         if (dayValue == 1){
-          alert("You were born on " +dayNames[0] + " and Your akan name is " +maleNames[0]+"!");
+         alert("You were born on " +dayNames[0] + " and Your akan name is " +maleNames[0]+"!");
         }
         else if(dayValue == 2){
-          alert("You were born on "+dayNames[1] + " and Your akan name is " +maleNames[1]+"!");
+            alert("You were born on "+dayNames[1] + " and Your akan name is " +maleNames[1]+"!");
         }
         else if(dayValue == 3){
-          alert("You were born on " +dayNames[2]+ " and Your akan name is " +maleNames[2]+"!");
+            alertL("You were born on " +dayNames[2]+ " and Your akan name is " +maleNames[2]+"!");
         }
         else if(dayValue == 4){
-          alert("You were born on "+dayNames[3] +  " and Your akan name is " +maleNames[3]+"!");
+            alert("You were born on "+dayNames[3] +  " and Your akan name is " +maleNames[3]+"!");
         }
         else if(dayValue == 5){
-          alert("You were born on "+dayNames[4] +  " and Your akan name is " +maleNames[4]+"!");
+            alert("You were born on "+dayNames[4] +  " and Your akan name is " +maleNames[4]+"!");
         }
         else if(dayValue == 6){
-          alert("You were born on "+dayNames[5] +  " and Your akan name is " +maleNames[5]+"!");
+            alert("You were born on "+dayNames[5] +  " and Your akan name is " +maleNames[5]+"!");
         }
         else if(dayValue == -0){
-          alert("You were born on "+dayNames[6] + " and Your akan name is " +maleNames[6]+"!");
+            alert("You were born on "+dayNames[6] + " and Your akan name is " +maleNames[6]+"!");
         }
       break;
       case "female":
         if (dayValue == 1){
-          alert("You were born on "+dayNames[0] + " and Your akan name is  " +femaleNames[0]+"!");
+            alert("You were born on "+dayNames[0] + " and Your akan name is  " +femaleNames[0]+"!");
         }
         else if(dayValue == 2){
-          alert("You were born on " +dayNames[1] + " and Your akan name is " +femaleNames[1]+"!");
+            alert("You were born on " +dayNames[1] + " and Your akan name is " +femaleNames[1]+"!");
         }
         else if(dayValue == 3){
-          alert("You were born on " +dayNames[2] + " and Your akan name is " +femaleNames[2]+"!");
+            alert("You were born on " +dayNames[2] + " and Your akan name is " +femaleNames[2]+"!");
         }
         else if(dayValue == 4){
-          alert("You were born on " +dayNames[3] + " and Your akan name is " +femaleNames[3]+"!");
+            alert("You were born on " +dayNames[3] + " and Your akan name is " +femaleNames[3]+"!");
         }
         else if(dayValue == 5){
-          alert("You were born on " +dayNames[4] + " and Your akan name is " +femaleNames[4]+"!");
+            alert("You were born on " +dayNames[4] + " and Your akan name is " +femaleNames[4]+"!");
         }
         else if(dayValue == 6){
-          alert("You were born on " +dayNames[5] + " and Your akan name is " + femaleNames[5]+"!");
+            alertL("You were born on " +dayNames[5] + " and Your akan name is " + femaleNames[5]+"!");
         }else if(dayValue == -0){
-          alert("You were born on " +dayNames[6] + " and Your akan name is " +femaleNames[6]+"!");
+            alert("You were born on " +dayNames[6] + " and Your akan name is " +femaleNames[6]+"!");
         }
       break
       default:
