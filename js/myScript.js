37f383aee3892021cdb3840b0638807d236be358
Joseph-Odhiambo <67067314+Joseph-Odhiambo@users.noreply.github.com>
2020-07-19 13:27:32 +0300

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -8,6 +8,7 @@ var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
+
 // input vadidation
 function validate() {
   var genders = document.getElementsByName("gender");
